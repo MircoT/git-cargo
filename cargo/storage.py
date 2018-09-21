f5e42ba908d9ecb540d8b9fcabd83292f8310269
MircoT <mirco.theone@gmail.com>
2018-09-21 10:53:44 +0200

    Add default init

--- cargo/storage.py
+++ cargo/storage.py
@@ -5,6 +5,20 @@ from os import environ, mkdir, path
 
 class Manager(metaclass=ABCMeta):
 
+    def __init__(self, source=None, target=None, config_file_name='manager.ini'):
+        self.__config_file_name = config_file_name
+        self.__config = configparser.ConfigParser()
+
+        if config_file_name and path.isfile(config_file_name):
+            with open(config_file_name) as config_file:
+                self.__config.read(config_file)
+            self.__source = self.__config['default']['source']
+            self.__target = self.__config['default']['target']
+
+        self.__source = source
+        self.__target = target
+
+
     @abstractmethod
     def push(self, filename):
         """Push a file on remote repo."""
@@ -24,15 +38,34 @@ class Manager(metaclass=ABCMeta):
     def list_remote(self):
         """List the remote files."""
         pass
-    
-    @abstractmethod
-    def configure(self):
-        """Configure the manager."""
-        pass
 
+    def configure(self, *args):
+        """Configure the manager."""
+        source = input("Insert your source folder path: ")
+        target = input(
+            "Insert your target folder name (the remote folder to use): ")
+        
+        additional_config = {}
+        for key in args:
+            additional_config[key] = input("Insert your default {}: ".format(key))
+
+        manager_config = configparser.ConfigParser()
+        manager_config['default'] = {
+            'source': source,
+            'target': target
+        }
+
+        for key, value in additional_config.items():
+            manager_config['default'][key] = value
+
+        with open(self.__config_file_name, "w") as manager_config_file:
+            manager_config.write(manager_config_file)
 
 class S3Manager(Manager):
 
+    def __init__(self, source=None, target=None, config_file_name='manager_s3.ini'):
+        super().__init__(source, target, config_file_name)
+
     def push(self, filename):
         """Push a file on remote repo."""
         pass
@@ -48,7 +81,7 @@ class S3Manager(Manager):
     def list_remote(self):
         """List the remote files."""
         pass
-    
+
     def configure(self):
         """Configure the manager."""
 
@@ -60,15 +93,15 @@ class S3Manager(Manager):
             'aws_access_key_id': aws_access_key_id,
             'aws_secret_access_key': aws_secret_access_key
         }
-        
+
         user_home = environ['HOME']
         aws_folder = path.join(user_home, ".aws")
         if not path.isdir(aws_folder):
             mkdir(aws_folder)
-        
+
         with open(path.join(aws_folder, "credentials"), "w") as aws_credential_file:
             aws_credentials.write(aws_credential_file)
-        
+
         region = input("Insert your aws region: ")
 
         aws_config = configparser.ConfigParser()
@@ -78,3 +111,5 @@ class S3Manager(Manager):
 
         with open(path.join(aws_folder, "config"), "w") as aws_config_file:
             aws_config.write(aws_config_file)
+        
+        super().configure('bucket')
